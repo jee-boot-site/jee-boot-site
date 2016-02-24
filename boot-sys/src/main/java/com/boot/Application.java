99284b7795a8fcf046268e6aa8848d7b2dc2d62d
lm <631663525@qq.com>
2016-02-24 10:59:08 +0800

    add mybatis generator

---
+++ boot-sys/src/main/java/com/boot/Application.java
@@ -0,0 +1,19 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuzh
+ * @since 2015-12-12 18:22
+ */
+
+@SpringBootApplication
+public class Application  {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ boot-sys/src/main/java/com/boot/sys/dao/SysUserMapper.java
@@ -0,0 +1,7 @@
+package com.boot.sys.dao;
+
+import com.boot.sys.model.SysUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SysUserMapper extends Mapper<SysUser> {
+}
