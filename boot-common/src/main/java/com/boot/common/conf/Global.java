f3ad355ca610ebb7b36639ffc7ef364012aa074f
kings <963987632@qq.com>
2016-02-27 23:54:31 +0800

    公共配置

---
+++ boot-common/src/main/java/com/boot/common/conf/Global.java
@@ -0,0 +1,15 @@
+package com.boot.common.conf;
+
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by Ming on 2016/2/25.
+ */
+
+@PropertySource(value = "classpath:jeesite.properties")
+public class Global {
+    /**
+     * 当前对象实例
+     */
+    private static Global global = new Global();
+}
